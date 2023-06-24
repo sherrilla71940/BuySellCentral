fa7db2a1263f48d91c2d98392c531b04242948c5
Aaron <sherrilla71940@gmail.com>
2023-06-24 17:08:39 +0800

    [BE] get seller in stock products instead, user does not need to see products they no longer sell in their store. Maybe in the future I can add a feature where user has a seperate component to see their sold/removed from store products

--- server/src/routers/products.ts
+++ server/src/routers/products.ts
@@ -7,6 +7,7 @@ import {
   updateProduct,
   deleteAllProducts,
   getSellerProducts,
+  getSellerInStockProducts,
   getAllProducts,
 } from "../controllers/product-controller";
 
@@ -17,7 +18,7 @@ export default function productsRouter(router: express.Router) {
   router.get("/products", getListedAndInStockProducts); // mvp 3 done: gets all products where seller still exists
   // changed above endpoint to also only get products that are in stock, where quantity > 0
   router.delete("/products/:id", unlistProduct); // mvp 4 done: sets sellerid of a product to null, so products can still be accessed in transaction but are no longer listed
-  router.get("/sellers/:sid", getSellerProducts); //  ** Alejandra done ** gets all products a seller has listed, and searches by seller/user id
+  router.get("/sellers/:sid", getSellerInStockProducts); //  ** Alejandra done ** gets all products a seller has listed, and searches by seller/user id
   router.put("/products/:id", updateProduct); // added mvp so we can update seller product by product id
 
   router.delete("/products", deleteAllProducts); // deleting ALL products in the database
