137a61f76a84d7e7de23e89eb2722929ee42c319
Aaron <sherrilla71940@gmail.com>
2023-06-26 02:06:08 +0800

    app is lagging quite hard right now, it my have something to do with (setReRender) or updating quantity on each + or -

--- client/src/components/shopping-cart/checkoutFunction.ts
+++ client/src/components/shopping-cart/checkoutFunction.ts
@@ -2,12 +2,12 @@ import useCartSlice from "./ShoppingCart";
 
 import { renderProductsStore } from "../../zustand/should-refetch-slice";
 
-export async function checkout() {
-  // const cartItems = useCartSlice((state) => state.cartItems);
+// export async function checkout() {
+//   // const cartItems = useCartSlice((state) => state.cartItems);
 
-  const { shouldReRender, setRerender } = renderProductsStore();
+//   const { shouldReRender, setRerender } = renderProductsStore();
 
-  setRerender(!shouldReRender);
-  console.log("ok");
-  return;
-}
+//   setRerender(!shouldReRender);
+//   console.log("ok");
+//   return;
+// }
