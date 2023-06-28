9af4f54734063c18bd5251b0f49e30cb43cd7bbf
Aaron <sherrilla71940@gmail.com>
2023-06-28 20:15:30 +0800

    It seems that previously changing to use Promise.all has solved my state problem, now without a location.reload(), quantity still updated correctly after checkout. Will double check.

--- client/src/components/shopping-cart/ShoppingCart.tsx
+++ client/src/components/shopping-cart/ShoppingCart.tsx
@@ -147,7 +147,7 @@ export default function ShoppingCart() {
       emptyCart();
       closeCart();
       navigate("/");
-      location.reload();
+      // location.reload();
       // });
     }
   }
